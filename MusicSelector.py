77f7352c0a1a08e42cfeb77f69fab0ed9b326f38
jyuste <jonathanyuste@mines-ales.org>
2021-04-08 14:48:23 +0200

    App Done

--- MusicSelector.py
+++ MusicSelector.py
@@ -5,10 +5,16 @@ import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+import re
 
-
+owlready2.JAVA_EXE = "C:/Program Files (x86)/Common Files/Oracle/Java/javapath/java.exe"
 onto_path = "C:/Users/zyran/OneDrive/Documents/Cours/EMA/Web Semantique/App/Ontologie/BANDEORIGINAL.owl"
 onto = get_ontology(onto_path).load()
+
+#with onto:
+    #sync_reasoner()
+
+
 
 def MusicSelector():
     listResultCat=[]
@@ -16,100 +22,126 @@ def MusicSelector():
     listResultInstru=[]
     #str(x).split(".")[1] for x in
     if (categorieEntry.get()!="Pas de selection"):
-        print("Cat yes")
+        #print("Cat yes")
         catSearch = onto.search_one(iri = "*"+categorieEntry.get())
-        print(catSearch)
+        #print(catSearch)
         listResultCat=onto.search(type = onto.Musique, estDeType=onto.search(is_a = catSearch))
-        print(listResultCat)
+        #print(listResultCat)
     if (universEntry.get()!="Pas de selection"):
-        print("Uni yes")
+        #print("Uni yes")
         uniSearch = onto.search_one(iri = "*"+universEntry.get())
-        print(uniSearch)
+        #print(uniSearch)
         listResultUni=onto.search(type = onto.Musique, provientDe=onto.search(is_a = onto.Oeuvre, estDansUnivers=onto.search(is_a = uniSearch)))
-        print(listResultUni)
+        #print(listResultUni)
     if (instruEntry.get()!="Pas de selection"):
-        print("Instru yes")
+        #print("Instru yes")
         instruSearch = onto.search_one(iri = "*"+instruEntry.get())
-        print(instruSearch)
+        #print(instruSearch)
         listResultInstru=onto.search(type = onto.Musique, estFaitAvec=onto.search(is_a = instruSearch))
-        print(listResultInstru)
+        #print(listResultInstru)
 
-    print("Test label")
-    for elem in listResultUni:
-        print(elem.label)
-
-    #maxList=0
     listMusic=[]
-    #listOf=""
     listMusicCounter=[]
-    """
-    if ( (len(listResultCat) > len(listResultUni) ) and ( len(listResultCat) > len(listResultInstru) ) ):
-        maxList=len(listResultCat)
-        listOf="Cat"
-    elif ( (len(listResultUni) > len(listResultCat) ) and ( len(listResultUni) > len(listResultInstru) ) ):
-        maxList=len(listResultUni)
-        listOf="Uni"
-    else:
-        maxList=len(listResultInstru)
-        listOf="Instru"
-    """
+    listAuthor=[]
+    testPopular=populariteBool.get()
+    testTheme=themeBool.get()
 
-    #print("ListOf: ", listOf)
-    #print("maxList: ", maxList)
-    """
-    if listOf=="Cat":
-        for i in range(maxList):
-            if ( (listResultCat[i] in listResultUni) and (listResultCat[i] in listResultInstru)):
-                listMusic.append(listResultCat[i])
-                listMusicCounter.append(3)
-            elif ( (listResultCat[i] in listResultUni) or (listResultCat[i] in listResultInstru)):
-                listMusic.append(listResultCat[i])
-                listMusicCounter.append(2)
-            else:
+    #print("TESTPOP=",testPopular)
+    for i in range(len(listResultCat)):
+        if(testPopular and testTheme):
+            if(listResultCat[i].VueYoutube>=5 and listResultCat[i].isTheme==True):
+                #print("Both Pop and Theme: ",listResultCat[i])
                 listMusic.append(listResultCat[i])
                 listMusicCounter.append(1)
-    elif listOf=="Uni":
-        for i in range(maxList):
-            if ( (listResultUni[i] in listResultCat) and (listResultUni[i] in listResultInstru)):
-                listMusic.append(listResultUni[i])
-                listMusicCounter.append(3)
-            elif ( (listResultUni[i] in listResultCat) or (listResultUni[i] in listResultInstru)):
-                listMusic.append(listResultUni[i])
-                listMusicCounter.append(2)
+        elif(testPopular):
+            #print("CAT: ",listResultCat[i]," vue=",listResultCat[i].VueYoutube)
+            if(listResultCat[i].VueYoutube>=5):
+                #print("CAT: Test ok for: ",listResultCat[i])
+                listMusic.append(listResultCat[i])
+                listMusicCounter.append(1)
+        elif(testTheme):
+            if(listResultCat[i].isTheme==True):
+                listMusic.append(listResultCat[i])
+                listMusicCounter.append(1)
+        else:
+            #print("CAT: Else pour: ", listResultCat[i])
+            listMusic.append(listResultCat[i])
+            listMusicCounter.append(1)
+        #print("CAT:",listMusic)
+    for i in range(len(listResultUni)):
+        if(testPopular and testTheme):
+            if(listResultUni[i].VueYoutube>=5 and listResultUni[i].isTheme==True):
+                #print("Both Pop and Theme: ",listResultUni[i])
+                if(listResultUni[i] in listMusic):
+                    tracker=listMusic.index(listResultUni[i])
+                    listMusicCounter[tracker]+=1
+                else:
+                    listMusic.append(listResultUni[i])
+                    listMusicCounter.append(1)
+        elif(testPopular):
+            #print("UNI: ",listResultUni[i]," vue=",listResultUni[i].VueYoutube)
+            if(listResultUni[i].VueYoutube>=5):
+                #print("UNI: Test ok for: ",listResultUni[i])
+                if(listResultUni[i] in listMusic):
+                    tracker=listMusic.index(listResultUni[i])
+                    listMusicCounter[tracker]+=1
+                else:
+                    listMusic.append(listResultUni[i])
+                    listMusicCounter.append(1)
+        elif(testTheme):
+            if(listResultUni[i].isTheme==True):
+                if(listResultUni[i] in listMusic):
+                    tracker=listMusic.index(listResultUni[i])
+                    listMusicCounter[tracker]+=1
+                else:
+                    listMusic.append(listResultUni[i])
+                    listMusicCounter.append(1)
+        else:
+            #print("UNI: Else pour: ", listResultUni[i])
+            if(listResultUni[i] in listMusic):
+                tracker=listMusic.index(listResultUni[i])
+                listMusicCounter[tracker]+=1
             else:
                 listMusic.append(listResultUni[i])
                 listMusicCounter.append(1)
-    elif listOf=="Instru":
-            for i in range(maxList):
-                if ( (listResultInstru[i] in listResultCat) and (listResultInstru[i] in listResultUni)):
-                    listMusic.append(listResultInstru[i])
-                    listMusicCounter.append(3)
-                elif ( (listResultInstru[i] in listResultCat) or (listResultInstru[i] in listResultUni)):
-                    listMusic.append(listResultInstru[i])
-                    listMusicCounter.append(2)
+        #print("UNI:",listMusic)
+    for i in range(len(listResultInstru)):
+        if(testPopular and testTheme):
+            if(listResultInstru[i].VueYoutube>=5 and listResultInstru[i].isTheme==True):
+                #print("Both Pop and Theme: ",listResultInstru[i])
+                if(listResultInstru[i] in listMusic):
+                    tracker=listMusic.index(listResultInstru[i])
+                    listMusicCounter[tracker]+=1
                 else:
                     listMusic.append(listResultInstru[i])
                     listMusicCounter.append(1)
-    """
-
-    for i in range(len(listResultCat)):
-        listMusic.append(listResultCat[i].label)
-        listMusicCounter.append(1)
-    for i in range(len(listResultUni)):
-        if(listResultUni[i].label in listMusic):
-            tracker=listMusic.index(listResultUni[i].label)
-            listMusicCounter[tracker]+=1
+        elif(testPopular):
+            #print("INSTRU: ",listResultInstru[i]," vue=",listResultInstru[i].VueYoutube)
+            if(listResultInstru[i].VueYoutube>=5):
+                #print("INSTRU: Test ok for: ",listResultInstru[i])
+                if(listResultInstru[i] in listMusic):
+                    tracker=listMusic.index(listResultInstru[i])
+                    listMusicCounter[tracker]+=1
+                else:
+                    listMusic.append(listResultInstru[i])
+                    listMusicCounter.append(1)
+        elif(testTheme):
+            if(listResultInstru[i].isTheme==True):
+                if(listResultInstru[i] in listMusic):
+                    tracker=listMusic.index(listResultInstru[i])
+                    listMusicCounter[tracker]+=1
+                else:
+                    listMusic.append(listResultInstru[i])
+                    listMusicCounter.append(1)
         else:
-            listMusic.append(listResultUni[i].label)
-            listMusicCounter.append(1)
-    for i in range(len(listResultInstru)):
-        if(listResultInstru[i].label in listMusic):
-            tracker=listMusic.index(listResultInstru[i].label)
-            listMusicCounter[tracker]+=1
-        else:
-            listMusic.append(listResultInstru[i].label)
-            listMusicCounter.append(1)
-
+            #print("INSTRU: Else pour: ", listResultInstru[i])
+            if(listResultInstru[i] in listMusic):
+                tracker=listMusic.index(listResultInstru[i])
+                listMusicCounter[tracker]+=1
+            else:
+                listMusic.append(listResultInstru[i])
+                listMusicCounter.append(1)
+        #print("INSTRU:",listMusic)
 
     print(listMusic)
     print(listMusicCounter)
@@ -128,12 +160,59 @@ def MusicSelector():
     print(listMusic)
     print(listMusicCounter)
     clear_text()
+    count=0
+    musicsInPlaylist=0
+    if(tempsPlaylistVar.get()!=''):
+        tempsMax=tempsPlaylistVar.get()
+        minuteCumulee=0
+        secondeCumulee=0
+        for x in listMusic:
+            DureeMusique=str(x.Duree)
+            MinuteMusique=re.sub(r"\[","",str(DureeMusique).split(".")[0])
+            SecondeMusique=re.sub(r"\]","",str(DureeMusique).split(".")[1])
+            minuteCumulee+=int(MinuteMusique)
+            secondeCumulee+=int(SecondeMusique)
+            if(secondeCumulee>=60):
+                secondeCumulee-=60
+                minuteCumulee+=1
+            musicsInPlaylist+=1
+            if(minuteCumulee>int(tempsMax)):
+                break
+            #print(minuteCumulee,":",secondeCumulee)
+            #print("Minute de la musique: "+MinuteMusique)
+            #print("Seconde de la musique: "+SecondeMusique)
+
+
+    rate=0
+    if(categorieEntry.get()!="Pas de selection"):
+        rate+=1
+    if(universEntry.get()!="Pas de selection"):
+        rate+=1
+    if(instruEntry.get()!="Pas de selection"):
+        rate+=1
+
     for x in listMusic:
+        #print("Is",x,"a theme? ",x.isTheme)
+        count+=1
         at=listMusic.index(x)
-        foundRate=round(listMusicCounter[at]/3, 2)
-        textAAfficher=foundRate,x
+        foundRate=round(listMusicCounter[at]/rate, 2)
+        DureeMusique=str(x.Duree)
+        DureeMusique=re.sub(r"\.",":",DureeMusique)
+        Oeuvre=onto.search_one(type = onto.Oeuvre, ContientMusique=onto.search(is_a = x))
+        textAAfficher="("+str(foundRate)+") "+str(x.label)+" - "+str(Oeuvre.label)+" ("+DureeMusique+")"
+        textAAfficher=re.sub(r"\[|\]|'|\"","",textAAfficher)
         print(textAAfficher)
         insert_text(textAAfficher)
+        if(nbrMusiqueVar.get()!='') and (tempsPlaylistVar.get()!=''):
+            if(count>=int(nbrMusiqueVar.get())) or (count>=musicsInPlaylist):
+                break
+        elif(nbrMusiqueVar.get()!='') or (tempsPlaylistVar.get()!=''):
+            if(tempsPlaylistVar.get()!=''):
+                if(count>=musicsInPlaylist):
+                    break
+            elif(nbrMusiqueVar.get()!=''):
+                if(count>=int(nbrMusiqueVar.get())):
+                    break
 
 
 appScreen=Tk()
@@ -194,6 +273,8 @@ themeBool=IntVar()
 themeCB=Checkbutton(can, text="Theme only?", variable=themeBool, onvalue=1, offvalue=0, font="Calibri 16 bold")
 themeCB.grid(row=6, column=2)
 
+
+
 #Gestion bouton recherche
 def insert_text(text):
     resultArea.config(state=NORMAL)
@@ -219,13 +300,14 @@ def noCommandyet():
     resultArea.update()
 
 
+
 searchButton=Button(can,text="Rechercher",font="Calibri 16",overrelief ="ridge",command=MusicSelector)
 searchButton.grid(row=7, column=1, columnspan=2)
 
 #Display résultat
 scrollbar = Scrollbar(can)
-resultArea=Text(can, height=10, width=40, font="Calibri 16 bold", bd=4, relief=RIDGE, bg='#BBBBBB', state=DISABLED, yscrollcommand=scrollbar.set)
-scrollbar.grid(row=1, column=4, rowspan=7, padx=0, pady=0)
+resultArea=Text(can, height=10, width=50, font="Calibri 16 bold", bd=4, relief=RIDGE, bg='#BBBBBB', state=DISABLED, yscrollcommand=scrollbar.set)
+scrollbar.grid(row=1, column=4, rowspan=7, padx=0, pady=0, sticky=N+S)
 scrollbar.config(command=resultArea.yview)
 resultArea.grid(row=1, column=3, rowspan=7, padx=20, pady=20)
 
